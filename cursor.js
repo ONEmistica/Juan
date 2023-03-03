b142f308ada0be32abc3b8669da5cc03f7705c8a
Miguel <77378774+ONEmistica@users.noreply.github.com>
2023-03-04 00:02:16 +0800

    Add files via upload

---
+++ cursor.js
@@ -0,0 +1,13 @@
+const cursorRounded = document.querySelector('.cursor');
+
+const moveCursor = (e) => {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+
+    cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+
+
+}
+
+window.addEventListener('mousemove', moveCursor)
